c83a54a1f0bd2fb984862a87f5f08b93b889d6cf
q pary <q.pary@outlook.fr>
2020-03-16 10:33:58 +0100

    ex01

---
+++ JS/Exo1/exo1.js
@@ -0,0 +1,26 @@
+var app = {
+    init: function() {
+        console.log('init');
+        var button = document.getElementById('button');
+        button.addEventListener('click', app.writeInDiv);
+    },
+
+    writeInDiv: function() {
+        console.log('click');
+        input = document.getElementById('input');
+        inputVal = input.value;
+
+        select = document.getElementById('select');
+        selectVal = parseInt(select.value);
+
+        console.log(selectVal);
+
+        div = document.getElementById('write');
+
+        for(i = 0; i < selectVal; i++) {
+            div.innerHtml += ' ' + String(inputVal);
+        }
+    }
+};
+
+document.addEventListener('DOMContentLoaded', app.init);
